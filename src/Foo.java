2e4163af93f8aa5530bb6c045addddc045fb2e29
requ060524 <chrislee060524@gmail.com>
2025-09-18 17:03:22 +0900

    second commit

---
+++ src/Foo.java
@@ -0,0 +1,14 @@
+public class Foo { 
+    public static void main(String[] args) {
+      byte b = 127;
+      int i = 100;
+
+      System.out.println(b + i);
+      System.out.println(10/4);
+      System.out.println(10.0/4);
+      System.out.println((char)0x12340041);
+      System.out.println((int)2.9 + (int)1.8); // 2 + 1 = 3
+      System.out.println();
+    }
+
+}
